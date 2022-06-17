01f66a1693614556f81bdd6b9c2a507ed0eaa1bf
taylor-barron <taylor.barron989@gmail.com>
2022-06-17 16:50:28 -0400

    fixed some file structure, added get all years with data function

--- middleware/server.js
+++ middleware/server.js
@@ -7,13 +7,31 @@ const { mongoose, MongoClient } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGO_CLIENT
+const uri = process.env.MONGO_DB
 const client = new MongoClient(uri);
 
 //--------------------------------------------- GETs ------------------------------------------------//
 
+// Return all years that have data
+app.get('/years', (req, res) => {
+    const uri = process.env.MONGO_DB
+    const client = new MongoClient(uri);
 
-app.get('/', (req, res) => {
-    res.send("hi")
+    async function getYears(client) {
+
+        try {
+            await client.connect();
+            const years = await client.db("Years").collection("Years").find().toArray()
+            console.log(years)
+            res.json(years);
+        } catch(e) {
+            console.error(e)
+        } finally {
+            await client.close()
+        }
+    }
+
+    getYears(client)
 })
+
 app.listen(4000)
