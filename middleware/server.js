80a1a6f9d25ac57697e2663b476e26328cf4ab29
taylor-barron <taylor.barron989@gmail.com>
2022-06-17 17:21:17 -0400

    added get weeks and get games functions

--- middleware/server.js
+++ middleware/server.js
@@ -17,12 +17,11 @@ app.get('/years', (req, res) => {
     const uri = process.env.MONGO_DB
     const client = new MongoClient(uri);
 
-    async function getYears(client) {
+    async function getYears(client, res) {
 
         try {
             await client.connect();
             const years = await client.db("Years").collection("Years").find().toArray()
-            console.log(years)
             res.json(years);
         } catch(e) {
             console.error(e)
@@ -31,7 +30,52 @@ app.get('/years', (req, res) => {
         }
     }
 
-    getYears(client)
+    getYears(client, res)
+})
+
+// Return all weeks that have data. Res is array of objects. info you want is object.name
+app.post('/weeks', (req, res) => {
+    const uri = process.env.MONGO_DB
+    const client = new MongoClient(uri);
+    year = req.body.year
+
+    async function getWeeks(client, year, res) {
+
+        try {
+            await client.connect();
+            const weeks = await client.db(year).listCollections().toArray()
+            res.json(weeks)
+        } catch(e) {
+            console.error(e)
+        } finally {
+            await client.close()
+        }
+    }
+
+    getWeeks(client, year, res)
+})
+
+// gets games. returns as array of objects
+app.post('/games', (req, res) => {
+    const uri = process.env.MONGO_DB
+    const client = new MongoClient(uri);
+    year = req.body.year
+    week = req.body.week
+
+    async function getGames(client, year, week, res) {
+
+        try {
+            await client.connect();
+            const games = await client.db(year).collection(week).find().toArray()
+            res.json(games)
+        } catch(e) {
+            console.error(e)
+        } finally {
+            await client.close()
+        }
+    }
+
+    getGames(client, year, week, res)
 })
 
 app.listen(4000)
