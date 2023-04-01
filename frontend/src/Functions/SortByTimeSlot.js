03e5d95b44606d35f18355c8b1bbb6d5712849c7
taylor-barron <taylor.barron989@gmail.com>
2023-04-01 18:38:59 -0400

    updated sort by timeslot to deal with out of order time slots

--- frontend/src/Functions/SortByTimeSlot.js
+++ frontend/src/Functions/SortByTimeSlot.js
@@ -4,18 +4,6 @@ const SortByTimeSlot = (games) => {
     let diffTimeslots = [];
     let allTimeSlots = [];
 
-    // for (let i = 1; i < games.length; i++) {
-
-    //     if ( games[i].timeSlot === games[i-1].timeSlot ) {
-    //         timeSlot.push(games[i]);
-    //     } else {
-    //         allTimeSlots.push(timeSlot)
-    //         timeSlot = []
-    //         timeSlot.push(games[i])
-    //     }
-    // }
-    // allTimeSlots.push(timeSlot)
-
     games.forEach(obj => {
         
         let timeSlot = obj.timeSlot;
