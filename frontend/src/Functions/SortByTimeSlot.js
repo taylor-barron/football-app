1abe8acd1d910c54e284457c4da3adf32c5c200d
taylor-barron <taylor.barron989@gmail.com>
2023-04-01 18:35:30 -0400

    updated sort by timeslot to deal with out of order time slots

--- frontend/src/Functions/SortByTimeSlot.js
+++ frontend/src/Functions/SortByTimeSlot.js
@@ -1,22 +1,45 @@
 
 const SortByTimeSlot = (games) => {
 
-    let allTimeSlots = []
-    let timeSlot = [games[0]]
+    let diffTimeslots = [];
+    let allTimeSlots = [];
 
-    for (let i = 1; i < games.length; i++) {
+    // for (let i = 1; i < games.length; i++) {
 
-        if ( games[i].timeSlot === games[i-1].timeSlot ) {
-            timeSlot.push(games[i]);
-        } else {
-            allTimeSlots.push(timeSlot)
-            timeSlot = []
-            timeSlot.push(games[i])
+    //     if ( games[i].timeSlot === games[i-1].timeSlot ) {
+    //         timeSlot.push(games[i]);
+    //     } else {
+    //         allTimeSlots.push(timeSlot)
+    //         timeSlot = []
+    //         timeSlot.push(games[i])
+    //     }
+    // }
+    // allTimeSlots.push(timeSlot)
+
+    games.forEach(obj => {
+        
+        let timeSlot = obj.timeSlot;
+
+        if (!diffTimeslots.includes(timeSlot)) {
+            diffTimeslots.push(timeSlot);
         }
-    }
-    allTimeSlots.push(timeSlot)
+        
+    });
 
-    return allTimeSlots
+    for (let i = 0; i < diffTimeslots.length; i++) {
+
+        let tempArray = [];
+        
+        let arrayTimeslot = diffTimeslots[i];
+
+        games.filter(obj => obj.timeSlot === arrayTimeslot).forEach(obj => tempArray.push(obj));
+
+        allTimeSlots.push(tempArray);
+
+    }
+
+    return allTimeSlots;
+
 }
 
-export default SortByTimeSlot
+export default SortByTimeSlot;
